acc26e2f392b4f421e0bcedfbf9e52b4f7305b52
robynspark <robynpark@gwmail.gwu.edu>
2017-03-28 12:08:23 -0400

    Add files via upload

--- scrape.py
+++ scrape.py
@@ -1,64 +1,27 @@
 import csv
 import requests
 from BeautifulSoup import BeautifulSoup
-# encoding=utf8
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
 
-url = 'https://columbian.gwu.edu/2015-2016'
-response = requests.get(url)
-html = response.content
-
-soup = BeautifulSoup(html)
-table = soup.find('table')
-
+years = ['2010-2011', '2011-2012', '2012-2013', '2013-2014', '2014-2015', '2015-2016']
 list_of_rows = []
-for row in table.findAll('tr')[1:-1]:
-    list_of_cells = []
-    for cell in row.findAll('td'):
-        list_of_cells.append(cell.text.encode('utf-8'))
-    list_of_rows.append(list_of_cells)
 
-outfile = open("college.csv", "wb")
+for year in years:
+	print year
+	response = requests.get('https://columbian.gwu.edu/' + year)
+	html = response.content
+
+	soup = BeautifulSoup(html)
+	table = soup.find('table')
+
+	for row in table.findAll('tr')[1:]:
+		list_of_cells = []
+    	list_of_cells.append(year)
+    	for cell in row.findAll('td'):
+    		list_of_cells.append(cell.text.encode('utf-8'))
+    	list_of_rows.append(list_of_cells)
+
+outfile = open("grants.csv", "wb")
 writer = csv.writer(outfile)
-writer.writerow(["Department", "Faculty", "Sponsor", "Title"])
+writer.writerow(["Year", "Department", "Faculty", "Sponsor", "Title"])
 writer.writerows(list_of_rows)
 
-url = 'https://columbian.gwu.edu/2014-2015'
-response = requests.get(url)
-html = response.content
-
-soup = BeautifulSoup(html)
-table = soup.find('table')
-
-list_of_rows = []
-for row in table.findAll('tr')[1:-1]:
-    list_of_cells = []
-    for cell in row.findAll('td'):
-        list_of_cells.append(cell.text.encode('utf-8'))
-    list_of_rows.append(list_of_cells)
-
-outfile = open("college.csv", "wb")
-writer = csv.writer(outfile)
-writer.writerow(["Department", "Faculty", "Sponsor", "Title"])
-writer.writerows(list_of_rows)
-
-url = 'https://columbian.gwu.edu/2013-2014'
-response = requests.get(url)
-html = response.content
-
-soup = BeautifulSoup(html)
-table = soup.find('table')
-
-list_of_rows = []
-for row in table.findAll('tr')[1:-1]:
-    list_of_cells = []
-    for cell in row.findAll('td'):
-        list_of_cells.append(cell.text.encode('utf-8'))
-    list_of_rows.append(list_of_cells)
-
-outfile = open("college.csv", "wb")
-writer = csv.writer(outfile)
-writer.writerow(["Department", "Faculty", "Sponsor", "Title"])
-writer.writerows(list_of_rows)
